d2e345f6ca9e8532593ba1873305019c41183712
NoahSpencerCode <noah@BigMac.askey.com>
2022-08-26 10:46:48 -0700

    Changed replicated root to root

--- Unpack.lua
+++ Unpack.lua
@@ -4,8 +4,6 @@ main.ClientRoot.Parent = game.StarterPlayer.StarterPlayerScripts
 
 main.Root.Parent = game.ReplicatedStorage
 
-main.ReplicatedRoot.Parent = game.ReplicatedStorage
-
 main.ServerRoot.Parent = game.ServerScriptService
 
 main:Destroy()
