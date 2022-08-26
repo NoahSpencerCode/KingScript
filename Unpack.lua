6dc5817b527f565fee5b4cfc7d844256b452968d
NoahSpencerCode <noah@BigMac.askey.com>
2022-08-26 10:45:10 -0700

    Files and Unpack code

---
+++ Unpack.lua
@@ -0,0 +1,11 @@
+local main = script.Parent
+
+main.ClientRoot.Parent = game.StarterPlayer.StarterPlayerScripts
+
+main.Root.Parent = game.ReplicatedStorage
+
+main.ReplicatedRoot.Parent = game.ReplicatedStorage
+
+main.ServerRoot.Parent = game.ServerScriptService
+
+main:Destroy()
